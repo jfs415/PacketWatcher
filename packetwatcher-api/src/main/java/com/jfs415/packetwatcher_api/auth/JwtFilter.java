3a030f6b124d43216a56bec5bef846e3e4e5ce96
Jon Healy <jhealy278@gmail.com>
2023-01-28 20:18:06 -0500

    Add api module authentication classes

---
+++ packetwatcher-api/src/main/java/com/jfs415/packetwatcher_api/auth/JwtFilter.java
@@ -0,0 +1,57 @@
+package com.jfs415.packetwatcher_api.auth;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.HttpHeaders;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.jfs415.packetwatcher_api.PacketWatcherApi;
+
+import io.jsonwebtoken.lang.Strings;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@Override
+	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		// Get authorization header and validate
+		final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+		if (Strings.hasText(header) || !header.startsWith("Bearer ")) {
+			chain.doFilter(request, response);
+			return;
+		}
+
+		// Get jwt token and validate (bearer dsa.sadasdasdsa -> ([bearer] [dsa.sadasdasdsa])
+		final String token = header.split(" ")[1].trim();
+		
+		// Get user identity and set it on the spring security context
+		UserDetails userDetails = PacketWatcherApi.getUserService().getUserByUsername(jwtUtil.getUsername(token)).orElse(null);
+		
+		if (userDetails == null || !jwtUtil.validateToken(token, userDetails)) {
+			chain.doFilter(request, response);
+			return;
+		}
+		
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+		authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		chain.doFilter(request, response);
+	}
+
+}

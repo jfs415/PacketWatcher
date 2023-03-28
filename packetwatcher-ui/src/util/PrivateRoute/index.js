7688adffc8ea7fb700367ca1d9d1bbd6b0dadbf6
Jon Healy <jhealy278@gmail.com>
2023-03-27 20:54:15 -0400

    Add Packetwatcher-UI util and styles

---
+++ packetwatcher-ui/src/util/PrivateRoute/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useLocalState } from "../LocalStorage";
+import { Navigate } from "react-router";
+
+const PrivateRoute = ({ children }) => {
+
+    const [jwt, setJwt] = useLocalState("", "jwt");
+
+    return jwt ? children : <Navigate to={ "/" }/>;
+};
+
+export default PrivateRoute;

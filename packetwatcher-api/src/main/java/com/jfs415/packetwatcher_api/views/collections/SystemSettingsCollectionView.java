d239260fb9f060741f4081e8a02a77b14769b049
Jon Healy <jhealy278@gmail.com>
2023-04-29 12:06:02 -0400

    Add the remaining views for other tables in the model

---
+++ packetwatcher-api/src/main/java/com/jfs415/packetwatcher_api/views/collections/SystemSettingsCollectionView.java
@@ -0,0 +1,22 @@
+package com.jfs415.packetwatcher_api.views.collections;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Immutable;
+
+import com.jfs415.packetwatcher_api.views.SystemSettingView;
+
+@Immutable
+public class SystemSettingsCollectionView {
+
+	private final List<SystemSettingView> settings;
+
+	public SystemSettingsCollectionView(List<SystemSettingView> settings) {
+		this.settings = settings;
+	}
+
+	public List<SystemSettingView> getSettings() {
+		return settings;
+	}
+
+}

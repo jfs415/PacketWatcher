9c329ebecbb393b356e1cae3bf10161260404c82
Jon Healy <jhealy278@gmail.com>
2023-05-13 00:14:17 -0400

    Implement parent repository interface for the current Event repositories to reduce redundancy. Build out Event repository API a bit more.

--- packetwatcher-api/src/main/java/com/jfs415/packetwatcher_api/model/repositories/AuthenticationEventRepository.java
+++ packetwatcher-api/src/main/java/com/jfs415/packetwatcher_api/model/repositories/AuthenticationEventRepository.java
@@ -1,11 +1,10 @@
 package com.jfs415.packetwatcher_api.model.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.jfs415.packetwatcher_api.model.events.AuthenticationEvent;
 
 @Repository
-public interface AuthenticationEventRepository extends JpaRepository<AuthenticationEvent, String> {
-
+public interface AuthenticationEventRepository extends PacketWatcherEventRepository<AuthenticationEvent, String> {
+	//Intentionally empty, all used methods are inherited currently. Type specific ones can be added here.
 }
---
+++ packetwatcher-api/src/main/java/com/jfs415/packetwatcher_api/model/repositories/PacketWatcherEventRepository.java
@@ -0,0 +1,44 @@
+package com.jfs415.packetwatcher_api.model.repositories;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import com.jfs415.packetwatcher_api.model.events.EventMappedSuperclass;
+
+@NoRepositoryBean
+public interface PacketWatcherEventRepository<T extends EventMappedSuperclass, E extends Serializable> extends JpaRepository<T, E> {
+
+	Iterable<T> findAllByUsername(String username);
+
+	Iterable<T> findAllByIpAddress(String ipAddress);
+
+	Iterable<T> findAllByTimestampBefore(Timestamp before);
+
+	Iterable<T> findAllByTimestampAfter(Timestamp after);
+
+	Iterable<T> findAllByTimestampBetween(Timestamp start, Timestamp end);
+
+	Iterable<T> findAllByUsernameAndTimestampBefore(String username, Timestamp before);
+
+	Iterable<T> findAllByUsernameAndTimestampAfter(String username, Timestamp after);
+
+	Iterable<T> findAllByUsernameAndTimestampBetween(String username, Timestamp start, Timestamp stop);
+
+	Iterable<T> findAllByIpAddressAndTimestampBefore(String ipAddress, Timestamp before);
+
+	Iterable<T> findAllByIpAddressAndTimestampBetween(String ipAddress, Timestamp start, Timestamp end);
+
+	Iterable<T> findAllByIpAddressAndUsername(String ipAddress, String username);
+	
+	Iterable<T> findAllByIpAddressAndTimestampAfter(String ipAddress, Timestamp after);
+
+	Iterable<T> findAllByUsernameAndIpAddressAndTimestampBefore(String username, String ipAddress, Timestamp timestamp);
+
+	Iterable<T> findAllByUsernameAndIpAddressAndTimestampBetween(String username, String ipAddress, Timestamp start, Timestamp end);
+
+	Iterable<T> findAllByUsernameAndIpAddressAndTimestampAfter(String username, String ipAddress, Timestamp after);
+
+}

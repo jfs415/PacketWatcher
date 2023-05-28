168ccb4aca6f5cf2b6c2e6910a79dc8737bf5ede
Jon Healy <jhealy278@gmail.com>
2023-05-28 17:17:16 -0400

    Refactor core properties tasks manager logging and config properties reloading.

--- packetwatcher-core/src/main/java/com/jfs415/packetwatcher_core/PacketWatcherCoreTasks.java
+++ packetwatcher-core/src/main/java/com/jfs415/packetwatcher_core/PacketWatcherCoreTasks.java
@@ -2,6 +2,8 @@ package com.jfs415.packetwatcher_core;
 
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,14 @@ import com.jfs415.packetwatcher_core.services.PacketService;
 
 @Component
 public class PacketWatcherCoreTasks {
+	
+	private final Logger logger = LoggerFactory.getLogger(PacketWatcherCoreTasks.class);
 
 	@Autowired
 	private PacketService packetService;
+
+	@Autowired
+	private CorePropertiesManager corePropertiesManager;
 
 	@Scheduled(fixedDelay = 33, timeUnit = TimeUnit.SECONDS)
 	public void processSaveQueues() {
@@ -23,13 +30,13 @@ public class PacketWatcherCoreTasks {
 	@Scheduled(fixedDelay = 1, timeUnit = TimeUnit.DAYS)
 	public void createTestPacket() {
 		packetService.savePacketRecord(FlaggedPacketRecord.createTestPacket());
-		PacketWatcherCore.debug("CREATED TEST PACKET");
+		logger.debug("Created test packet");
 	}
 
 	@Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
 	public void refreshConfig() {
-		PacketWatcherCore.getCoreConfigProperties().refresh();
-		PacketWatcherCore.debug("PacketWatcher config has been refreshed");
+		corePropertiesManager.refresh();
+		logger.debug("PacketWatcher config has been refreshed");
 	}
 
 }

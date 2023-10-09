cbf77ef99a406f1c18ec61c2c3991cacddff5ea6
Jon Healy <jhealy278@gmail.com>
2023-10-08 20:52:21 -0400

    refactor(core): Refactor Duplicate Lines In IpComparator

--- packetwatcher-core/src/main/java/com/jfs415/packetwatcher_core/filter/IpComparator.java
+++ packetwatcher-core/src/main/java/com/jfs415/packetwatcher_core/filter/IpComparator.java
@@ -6,98 +6,43 @@ public class IpComparator implements RangedComparator<String, String, String> {
 
     @Override
     public boolean isInRangeInclusive(String value, String floor, String ceiling) {
-        if (value == null || floor == null || ceiling == null || value.isBlank() || floor.isBlank() || ceiling.isBlank()) {
+        if (value != null && floor != null && ceiling != null && !value.isBlank() && !floor.isBlank() && !ceiling.isBlank()) {
+            return validate(value, floor, ceiling, TokenizerEvaluator.RANGE_INCLUSIVE);
+        } else {
             return false;
         }
-
-        StringTokenizer valueTokenizer = new StringTokenizer(value, ".");
-        StringTokenizer floorTokenizer = new StringTokenizer(floor, ".");
-        StringTokenizer ceilingTokenizer = new StringTokenizer(ceiling, ".");
-
-        if (valueTokenizer.countTokens() != floorTokenizer.countTokens() || valueTokenizer.countTokens() != ceilingTokenizer.countTokens()) {
-            return false;
-        }
-
-        while (valueTokenizer.hasMoreTokens()) {
-            int valueOctet = Integer.parseInt(valueTokenizer.nextToken());
-            int floorOctet = Integer.parseInt(floorTokenizer.nextToken());
-            int ceilingOctet = Integer.parseInt(ceilingTokenizer.nextToken());
-
-            if (valueOctet < floorOctet || valueOctet > ceilingOctet) {
-                return false;
-            }
-
-        }
-
-        return true;
     }
 
     @Override
     public boolean isInRangeExclusive(String value, String floor, String ceiling) {
-        if (value == null || floor == null || ceiling == null || value.isBlank() || floor.isBlank() || ceiling.isBlank()) {
+        if (value != null && floor != null && ceiling != null && !value.isBlank() && !floor.isBlank() && !ceiling.isBlank()) {
+            return validate(value, floor, ceiling, TokenizerEvaluator.RANGE_EXCLUSIVE);
+        } else {
             return false;
         }
-
-        StringTokenizer valueTokenizer = new StringTokenizer(value, ".");
-        StringTokenizer floorTokenizer = new StringTokenizer(floor, ".");
-        StringTokenizer ceilingTokenizer = new StringTokenizer(ceiling, ".");
-
-        if (valueTokenizer.countTokens() != floorTokenizer.countTokens() || valueTokenizer.countTokens() != ceilingTokenizer.countTokens()) {
-            return false;
-        }
-
-        while (valueTokenizer.hasMoreTokens()) {
-            int valueOctet = Integer.parseInt(valueTokenizer.nextToken());
-            int floorOctet = Integer.parseInt(floorTokenizer.nextToken());
-            int ceilingOctet = Integer.parseInt(ceilingTokenizer.nextToken());
-
-            if (!valueTokenizer.hasMoreTokens() && (valueOctet <= floorOctet || valueOctet >= ceilingOctet)) { //Only last octet is bounded
-                return false;
-            } else if (valueTokenizer.countTokens() != floorTokenizer.countTokens() || valueTokenizer.countTokens() != ceilingTokenizer.countTokens()) {
-                return false;
-            }
-
-        }
-
-        return true;
     }
 
     @Override
     public boolean isInRangeLowerExclusiveUpperInclusive(String value, String floor, String ceiling) {
-        if (value == null || floor == null || ceiling == null || value.isBlank() || floor.isBlank() || ceiling.isBlank()) {
+        if (value != null && floor != null && ceiling != null && !value.isBlank() && !floor.isBlank() && !ceiling.isBlank()) {
+            return validate(value, floor, ceiling, TokenizerEvaluator.RANGE_LOWER_EXCLUSIVE_UPPER_INCLUSIVE);
+        } else {
             return false;
         }
 
-        StringTokenizer valueTokenizer = new StringTokenizer(value, ".");
-        StringTokenizer floorTokenizer = new StringTokenizer(floor, ".");
-        StringTokenizer ceilingTokenizer = new StringTokenizer(ceiling, ".");
-
-        if (valueTokenizer.countTokens() != floorTokenizer.countTokens() || valueTokenizer.countTokens() != ceilingTokenizer.countTokens()) {
-            return false;
-        }
-
-        while (valueTokenizer.hasMoreTokens()) {
-            int valueOctet = Integer.parseInt(valueTokenizer.nextToken());
-            int floorOctet = Integer.parseInt(floorTokenizer.nextToken());
-            int ceilingOctet = Integer.parseInt(ceilingTokenizer.nextToken());
-
-            if (!valueTokenizer.hasMoreTokens() && (valueOctet <= floorOctet || valueOctet > ceilingOctet)) { //Only last octet is bounded
-                return false;
-            } else if (valueTokenizer.countTokens() != floorTokenizer.countTokens() || valueTokenizer.countTokens() != ceilingTokenizer.countTokens()) {
-                return false;
-            }
-
-        }
-
-        return true;
     }
 
     @Override
     public boolean isInRangeLowerInclusiveUpperExclusive(String value, String floor, String ceiling) {
-        if (value == null || floor == null || ceiling == null || value.isBlank() || floor.isBlank() || ceiling.isBlank()) {
+        if (value != null && floor != null && ceiling != null && !value.isBlank() && !floor.isBlank() && !ceiling.isBlank()) {
+            return validate(value, floor, ceiling, TokenizerEvaluator.RANGE_LOWER_INCLUSIVE_UPPER_EXCLUSIVE);
+        } else {
             return false;
         }
 
+    }
+
+    private boolean validate(String value, String floor, String ceiling, TokenizerEvaluator evaluator) {
         StringTokenizer valueTokenizer = new StringTokenizer(value, ".");
         StringTokenizer floorTokenizer = new StringTokenizer(floor, ".");
         StringTokenizer ceilingTokenizer = new StringTokenizer(ceiling, ".");
@@ -106,20 +51,90 @@ public class IpComparator implements RangedComparator<String, String, String> {
             return false;
         }
 
-        while (valueTokenizer.hasMoreTokens()) {
-            int valueOctet = Integer.parseInt(valueTokenizer.nextToken());
-            int floorOctet = Integer.parseInt(floorTokenizer.nextToken());
-            int ceilingOctet = Integer.parseInt(ceilingTokenizer.nextToken());
+        return evaluator.processTokenizers(valueTokenizer, floorTokenizer, ceilingTokenizer);
+    }
 
-            if (!valueTokenizer.hasMoreTokens() && (valueOctet < floorOctet || valueOctet >= ceilingOctet)) { //Only last octet is bounded
-                return false;
-            } else if (valueTokenizer.countTokens() != floorTokenizer.countTokens() || valueTokenizer.countTokens() != ceilingTokenizer.countTokens()) {
-                return false;
+    private enum TokenizerEvaluator {
+        RANGE_INCLUSIVE {
+            @Override
+            public boolean processTokenizers(StringTokenizer valueTokenizer, StringTokenizer floorTokenizer, StringTokenizer ceilingTokenizer) {
+                while (valueTokenizer.hasMoreTokens()) {
+                    int valueOctet = Integer.parseInt(valueTokenizer.nextToken());
+                    int floorOctet = Integer.parseInt(floorTokenizer.nextToken());
+                    int ceilingOctet = Integer.parseInt(ceilingTokenizer.nextToken());
+
+                    if (valueOctet < floorOctet || valueOctet > ceilingOctet) {
+                        return false;
+                    }
+
+                }
+
+                return true;
             }
+        },
 
+        RANGE_EXCLUSIVE {
+            @Override
+            public boolean processTokenizers(StringTokenizer valueTokenizer, StringTokenizer floorTokenizer, StringTokenizer ceilingTokenizer) {
+                while (valueTokenizer.hasMoreTokens()) {
+                    int valueOctet = Integer.parseInt(valueTokenizer.nextToken());
+                    int floorOctet = Integer.parseInt(floorTokenizer.nextToken());
+                    int ceilingOctet = Integer.parseInt(ceilingTokenizer.nextToken());
+
+                    if ((!valueTokenizer.hasMoreTokens() && (valueOctet <= floorOctet || valueOctet >= ceilingOctet))
+                            || tokensNotAlike(valueTokenizer, floorTokenizer, ceilingTokenizer)) { //Only last octet is bounded
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        },
+
+        RANGE_LOWER_EXCLUSIVE_UPPER_INCLUSIVE {
+            @Override
+            public boolean processTokenizers(StringTokenizer valueTokenizer, StringTokenizer floorTokenizer, StringTokenizer ceilingTokenizer) {
+                while (valueTokenizer.hasMoreTokens()) {
+                    int valueOctet = Integer.parseInt(valueTokenizer.nextToken());
+                    int floorOctet = Integer.parseInt(floorTokenizer.nextToken());
+                    int ceilingOctet = Integer.parseInt(ceilingTokenizer.nextToken());
+
+                    if ((!valueTokenizer.hasMoreTokens() && (valueOctet <= floorOctet || valueOctet > ceilingOctet))
+                            || tokensNotAlike(valueTokenizer, floorTokenizer, ceilingTokenizer)) { //Only last octet is bounded
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        },
+
+        RANGE_LOWER_INCLUSIVE_UPPER_EXCLUSIVE {
+            @Override
+            public boolean processTokenizers(StringTokenizer valueTokenizer, StringTokenizer floorTokenizer, StringTokenizer ceilingTokenizer) {
+                while (valueTokenizer.hasMoreTokens()) {
+                    int valueOctet = Integer.parseInt(valueTokenizer.nextToken());
+                    int floorOctet = Integer.parseInt(floorTokenizer.nextToken());
+                    int ceilingOctet = Integer.parseInt(ceilingTokenizer.nextToken());
+
+                    if ((!valueTokenizer.hasMoreTokens() && (valueOctet < floorOctet || valueOctet >= ceilingOctet))
+                            || tokensNotAlike(valueTokenizer, floorTokenizer, ceilingTokenizer)) { //Only last octet is bounded
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        };
+
+        public boolean processTokenizers(StringTokenizer valueTokenizer, StringTokenizer floorTokenizer, StringTokenizer ceilingTokenizer) {
+            return valueTokenizer != null && floorTokenizer != null && ceilingTokenizer != null;
         }
 
-        return true;
+        protected boolean tokensNotAlike(StringTokenizer valueTokenizer, StringTokenizer floorTokenizer, StringTokenizer ceilingTokenizer) {
+            return valueTokenizer.countTokens() != floorTokenizer.countTokens() || valueTokenizer.countTokens() != ceilingTokenizer.countTokens();
+        }
+
     }
 
 }
